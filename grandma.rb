cfd0279f62c5e8ab5a3003447989de6a7e7506cb
Jordan Bacon <jmbacon92@gmail.com>
2019-06-17 04:31:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if "phrase" == "phrase".downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif "phrase" == "phrase".upcase
+  if "phrase" == "phrase".upcase
     return "NO, NOT SINCE 1938!"
   elsif "phrase" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
